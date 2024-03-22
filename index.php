5b7072741bb0296442a00c0117f630298c74003b
Francois Szczepkowski <francois.szcz@hotmail.fr>
2024-03-22 23:33:46 +0100

    ADD: gestion erreur

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
 // Routeur 
 require('controller/controller.php');
 
-if (isset($_GET['page'])) {
-    if ($_GET['page'] == 'accueil') {
+try {
+    if (isset($_GET['page'])) {
+        if ($_GET['page'] == 'accueil') {
+            home();
+        } elseif ($_GET['page'] == 'avis') {
+            testimonials();
+        } else {
+            throw new Exception("Cette pa n'existe pas ou a été supprimée");
+        }
+    } else {
         home();
-    } elseif ($_GET['page'] == 'avis') {
-        testimonials();
     }
-} else {
-    home();
+} catch (Exception $e) {
+    // die('Erreur : ' . $e->getMessage());
+
+    $error = $e->getMessage();
+    require('view/errorView.php');
 }
---
+++ view/errorView.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Vive le MVC !</title>
+    <link rel="stylesheet" href="public/design/default.css" />
+</head>
+
+<body>
+
+    <header>
+        <span>MVC</span>
+    </header>
+
+    <section class="container">
+        <h1>Oups</h1>
+        <p><?= $error ?></p>
+    </section>
+
+</body>
+
+</html>
